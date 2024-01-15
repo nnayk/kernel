8f1850b20d23f48da465f85eeaf65f2519d12114
CPE 454 <nakul.indus@gmail.com>
2024-01-15 10:13:56 -0800

    Abstracted away VGA API + updated makefile

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,4 @@
-void VGA_display_char(char);
-
+extern void VGA_display_char(char);
 void kmain()
 {
 	int loop = 0;
@@ -8,29 +7,3 @@ void kmain()
 	{
 	}
 }
-#if 1
-#define VGA_BASE 0xb8000
-static unsigned short *vgaBuff = (unsigned short*)VGA_BASE;
-static int width = 80;
-static int height = 20;
-static int cursor = 0;
-//static unsigned char color = FG(VGA_LIGHT_GREY) | BG(VGA_BLACK);
-void VGA_display_char(char c)
-{
-	vgaBuff[cursor] = (0x2f << 8) | c;
-#if 0
-	if (c == '\n') {
-		cursor = (LINE(cursor) + 1) * width;
-		if (cursor >= width*height)
-			scroll();
-	}
-	else if (c == '\r')
-		cursor = LINE(cursor);
-	else {
-		vgaBuff[cursor] = (color << 8) | c;
-		if ( (cursor % width) < (width - 1))
-			cursor++;
-	}
-#endif
-}
-#endif
