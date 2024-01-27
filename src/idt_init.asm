0f1a3a6cecb6e213d19bbb27fd0d5677c9f271cf
CPE 454 <nakul.indus@gmail.com>
2024-01-26 20:23:54 -0800

    Initial setup of idt

--- src/idt_init.asm
+++ src/idt_init.asm
@@ -1,9 +1,17 @@
 global idt_start
 
+section .bss
+    idt resb 4096
 section .text
 bits 64
 idt_start:
     mov rax, 0x2f592f412f4b2f4f
     mov qword [0xb8000], rax
     cli
-    ret
+idt_descriptor:
+    mov word [idt_descriptor], 4095
+    mov qword [idt_descriptor+2], idt
+mov rax,idt_descriptor
+lidt [rax]
+ret
+    
