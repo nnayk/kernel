fd5c25ab26d0b8ef3230b05a9b4bc014cc86112f
CPE 454 <nakul.indus@gmail.com>
2024-02-02 13:57:12 -0800

    Get rid of unneeded idtr stuff

--- src/idt_init.asm
+++ src/idt_init.asm
@@ -1,19 +1,4 @@
-;global idt_descriptor
 global idt
 section .bss
 idt:
     resb 4096
-;section .text
-;bits 64
-;idt_descriptor:
-   ;mov word [idt_descriptor], 4095
-   ;mov qword [idt_descriptor+2], idt2
-   ;mov rax,idt_descriptor
-   ;lidt [rax]
-   ;ret
- ;  dw 4095
- ;  hlt
- ;  dq idt
- ;  hlt
-;lidt [idt_descriptor]
-;ret
