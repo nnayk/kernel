d83a62316ac964e946111adf00225f2805d56e37
CPE 454 <nakul.indus@gmail.com>
2024-02-05 13:02:49 -0800

    Add error checking + int. safety to producer code

--- src/error.h
+++ src/error.h
@@ -9,5 +9,7 @@
 #define ERR_INPUT_SIZE -2
 #define ERR_MEMCPY -3
 #define ERR_SCROLL -4
+#define ERR_BUFF_EMPTY -5
+#define ERR_BUFF_FULL -6
 
 #define SUCCESS 1
--- src/shared_buff.h
+++ src/shared_buff.h
@@ -5,7 +5,11 @@
 */
 
 #define BUFF_SIZE 16
-struct State {
+typedef struct{
         char buff[BUFF_SIZE];
         char *consumer, *producer;
-};
+} State;
+
+ void init_state(struct State *)
+ int consumer_next(struct State *)
+ int producer_add_char(char, struct State *)
