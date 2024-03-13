cce69d7e4960a90cc6ffd9cfedc043d1ee74b8b8
CPE 454 <nakul.indus@gmail.com>
2024-03-13 08:01:46 -0700

    add md5 src

---
+++ src/md5.h
@@ -0,0 +1,21 @@
+#ifndef MD5_H
+#define MD5_H
+
+#include <stdint-gcc.h>
+#include <stddef.h>
+
+typedef struct{
+    uint64_t size;        // Size of input in bytes
+    uint32_t buffer[4];   // Current accumulation of hash
+    uint8_t input[64];    // Input to be used in the next step
+    uint8_t digest[16];   // Result of algorithm
+}MD5Context;
+
+void md5Init(MD5Context *ctx);
+void md5Update(MD5Context *ctx, uint8_t *input, size_t input_len);
+void md5Finalize(MD5Context *ctx);
+void md5Step(uint32_t *buffer, uint32_t *input);
+
+void md5String(char *input, uint64_t,uint8_t *result);
+
+#endif
